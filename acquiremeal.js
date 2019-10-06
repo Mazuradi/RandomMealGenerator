2c14f75bbf62bb04b8841cafdd166fcdf14e6e56
B.Giller <101624498@student.swin.edu.au>
2019-10-07 02:07:35 +1100

    extended functionality, use with ingredient quantity, tags and CSS

--- acquiremeal.js
+++ acquiremeal.js
@@ -3,7 +3,7 @@ const meal_container = document.getElementById('meal');
 
 //Event Listener.
 get_meal_btn.addEventListener('click', () => {
-	fetch('https://www.themealdb.com/api/json/v1/1/random.php.')
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
 		.then((res) => res.json())
 		.then((res) => {
 			createMeal(res.meals[0]);
@@ -27,7 +27,35 @@ const createMeal = (meal) => {
 		}
 	}
 
-	const newInnerHTML = ``;
+	const newInnerHTML = `
+		<div class="row">
+			<div class="columns five">
+				<img src="${meal.strMealThumb}" alt ="Meal's Image">
+				${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+				${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+				${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+				<h4>Ingredients:</h4>
+				<ul>
+					${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join('')}
+				</ul>
+			</div>
+			<div class="columns seven">
+				<h3>${meal.strMeal}</h3>
+				<p>${meal.strInstructions}</p>
+			</div>
+		</div>
+		${meal.strYoutube
+			? `<div class="row">
+								<h4>Video Recipe</h4>
+								<div class="videoWrapper">
+									<iframe width="420" height="315" 
+									src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+									</iframe>
+								</div>
+							</div>`
+			: ''}
+		
+	`;
 
 	meal_container.innerHTML = newInnerHTML;
 };
